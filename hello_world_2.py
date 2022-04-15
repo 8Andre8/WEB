9c17df281da22cb634f7c8ee1974d57d41513093
8Andre8 <boing1999@mail.ru>
2022-04-15 12:22:18 +0300

    лр2 - 'hello world' с помощью шаблона

--- hello_world_2.py
+++ hello_world_2.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Привет, Мир! (ЛР2)', text='Hello, World! (ЛР2)')
+    return render_template('index.html', title='Привет, Мир! (ЛР2)',
+                           text='Hello, World! (ЛР2)')
 
 
 if __name__ == '__main__':
